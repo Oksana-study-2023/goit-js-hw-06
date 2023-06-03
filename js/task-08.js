32cd8ee4aaa53ca4c99ad3bf5c67f792dba9d808
Oksana <oks46@ukr.net>
2023-06-03 18:35:48 +0300

    add task 7-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formEl = document.querySelector(`.login-form`);
+
+formEl.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    // const formData = new FormData(event.currentTarget);
+    // formData.forEach((value, name) => {
+    //     console.log(name);
+    //     console.log(value);
+    // })
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail, password,
+    };
+    if (mail !== "" & password !== "") {
+        console.log(formData);
+      formEl.reset();  
+    } else {
+        alert(`all fields must be filled`);
+    }
+
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector(`body`);
+const BtnChangeColorEl = document.querySelector(`.change-color`);
+const textBackgrondColorEl = document.querySelector(`.color`);
+
+BtnChangeColorEl.addEventListener(`click`, onBtnChangeColor);
+
+function onBtnChangeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textBackgrondColorEl.textContent = getRandomHexColor();
+}
+
