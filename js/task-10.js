33d897b2001613c8e8f05227c7e001e786c0c304
Oksana <oks46@ukr.net>
2023-06-03 19:35:40 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// const inputEl = document.querySelector(`input`);
+// const btnCreateEl = document.querySelector(`[data-create]`);
+// const btnDestroyEl = document.querySelector(`[data-destroy]`);
+
+// function onInputClick(event) {
+//   let amount = event.target.value;
+//   console.log(amount);
+// }
+// inputEl.addEventListener(`input`, onInputClick);
+// btnCreateEl.addEventListener(`click`, createBoxes);
+
+// function createBoxes(amount) {
+//   console.log(amount);
+// };
+
+
+
+
+
+
